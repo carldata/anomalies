457faf40a39edd3c592eb0bf94c774ef34a5022e
Tomasz Pichlak <tpichlak@gmail.com>
2018-03-19 13:33:19 +0100

    After fighting with loaders configuration added support for styles and fonts. Also added lot's of packages for further development.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
+import '../styles/bootstrap.min.css';
+
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { createHashHistory } from 'history'
 
 class HelloReactComponent extends React.Component {
   render() {
-    return <div>Hello and Welcome to React! Let's start a new journey !</div>
+    return <div className='container'> <div className='btn btn-success' > Hello and Welcome to React! Let's start a new journey ! </div> </div>
   }
 }
 
