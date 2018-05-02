35304c07239508761952b7786278e3a175f442c4
Tomasz Pichlak <tpichlak@gmail.com>
2018-05-02 14:39:28 +0200

    Playing with HpSlider.

--- src/anomalies-screen/index.tsx
+++ src/anomalies-screen/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Button, ButtonGroup, ControlLabel, Form, FormControl, FormGroup, Row, Col, Nav, NavItem, Navbar, NavDropdown, MenuItem } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
-import { convertHpSliderScss, convertHpTimeSeriesChartScss, HpTimeSeriesScroller, IHpTimeSeriesChartState } from 'time-series-scroller';
+import { convertHpSliderScss, convertHpTimeSeriesChartScss, HpTimeSeriesScroller, IHpTimeSeriesChartState, HpSlider, EnumHandleType, IUnixFromTo, handleMovedCallback } from 'time-series-scroller';
 import * as hpSliderScss from 'time-series-scroller/lib/out/sass/hp-slider.scss';
 import * as hpTimeSeriesChartScss from 'time-series-scroller/lib/out/sass/hp-time-series-chart.scss';
 import { IState } from '../state';
@@ -14,9 +14,12 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { AddChannelModal } from './controls/add-channel-control';
 import * as dateFns from 'date-fns';
 import * as _ from 'lodash';
+import { IDomain, IHpSliderHandleValues } from 'time-series-scroller/lib/out/hp-slider/interfaces';
 
 interface IAnomaliesComponentProps {
-  chartState: IHpTimeSeriesChartState;
+  mainChartState: IHpTimeSeriesChartState;
+  finalChartState?: IHpTimeSeriesChartState;
+  supportingChannels?: { site: string, channel: string, IHpTimeSeriesChartState }[];
   gridState: IDataGridState;
   project: IProject;
 }
@@ -28,9 +31,14 @@ interface IAnomaliesComponentActionCreators {
 }
 
 interface IAnomaliesComponentState {
+  mainChartState?: IHpTimeSeriesChartState;
+  finalChartState?: IHpTimeSeriesChartState;
+  supportingChannels?: { site: string, channel: string, chartState: IHpTimeSeriesChartState }[];
   showModal: boolean;
   startDate: string;
   endDate: string;
+  windowUnixFrom: number;
+  windowUnixTo: number;
 }
 
 class AnomaliesComponent extends React.Component<IAnomaliesComponentProps & IAnomaliesComponentActionCreators, IAnomaliesComponentState> {
@@ -40,8 +48,14 @@ class AnomaliesComponent extends React.Component<IAnomaliesComponentProps & IAno
       showModal: false,
       startDate: dateFns.format(dateFns.subMonths(dateFns.startOfDay(new Date()), 3), 'YYYY-MM-DDTHH:mm:ss'),
       endDate: dateFns.format(dateFns.startOfDay(new Date()), 'YYYY-MM-DDTHH:mm:ss'),
+      windowUnixFrom: props.mainChartState.dateRangeUnixFrom,
+      windowUnixTo: props.mainChartState.dateRangeUnixTo,
     }
   };
+
+  componentWillReceiveProps(nextProps: IAnomaliesComponentProps & IAnomaliesComponentActionCreators) {
+
+  }
 
   public render() {
     return <div>
@@ -80,7 +94,7 @@ class AnomaliesComponent extends React.Component<IAnomaliesComponentProps & IAno
             <FormControl.Static>{_.isEmpty(this.props.project) ? ' ' : this.props.project.final}</FormControl.Static>
           </FormGroup>
           {' '}
-          <FormGroup className='pull-right'>
+          <FormGroup>
             <ControlLabel>Start Date:</ControlLabel>
             {' '}
             <FormControl type='text' value={this.state.startDate} onChange={(e) => this.setState({ startDate: (e.target as HTMLInputElement).value })}></FormControl>
@@ -91,24 +105,55 @@ class AnomaliesComponent extends React.Component<IAnomaliesComponentProps & IAno
           </FormGroup>
         </Form>
 
-        <Form>
-          <FormGroup style={{ maxHeight: 300 }}>
-                  <HpTimeSeriesScroller
-                    chartState={this.props.chartState}
-                    sliderScss={convertHpSliderScss(hpSliderScss)}
-                    timeSeriesChartScss={convertHpTimeSeriesChartScss(hpTimeSeriesChartScss)}
-                    fitToParentSize={true}>
-                  </HpTimeSeriesScroller>
-           </FormGroup>
-          <FormGroup>
-              <Button style={{ marginTop: 100 }} className='btn-primary' onClick={() => this.setState({ showModal: true })} >Add Channel</Button>
-           </FormGroup>
-        </Form>
+        {/* <Row style={{ maxHeight: 300 }} >
+          <Col sm={12}>
+            <HpTimeSeriesScroller
+              chartState={this.props.mainChartState}
+              sliderScss={convertHpSliderScss(hpSliderScss)}
+              timeSeriesChartScss={convertHpTimeSeriesChartScss(hpTimeSeriesChartScss)}
+              fitToParentSize={true}>
+            </HpTimeSeriesScroller>
+          </Col>
+        </Row> */}
+
+        <Row style={{ minHeight: 80, marginLeft: 0 }}>
+          <Col sm={12}>
+            <HpSlider
+              scss={convertHpSliderScss(hpSliderScss)}
+              domain={{ domainMin: this.props.mainChartState.dateRangeUnixFrom, domainMax: this.props.mainChartState.dateRangeUnixTo } as IDomain<number>}
+              displayDragBar={true}
+              handleValues={{ left: this.state.windowUnixFrom, right: this.state.windowUnixTo } as IHpSliderHandleValues<number>}
+              handleMoved={(value: number | number[], type: EnumHandleType) => {
+                let { windowUnixFrom, windowUnixTo } = handleMovedCallback(value, type, { 
+                  windowUnixFrom: this.state.windowUnixFrom, 
+                  windowUnixTo: this.state.windowUnixTo } as IUnixFromTo)
+
+                  this.setState({
+                    windowUnixFrom: windowUnixFrom,
+                    windowUnixTo: windowUnixTo,
+                  })
+              }}
+              fitToParent={{ toWidth: true, offsetWidth: 35 }}
+            ></HpSlider>
+
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={12}>
+            <Button style={{ marginTop: 100 }} className='btn-primary' onClick={() => this.setState({ showModal: true })} >Add Channel</Button>
+          </Col>
+        </Row>
+        {/* </Form> */}
         <Row>
           <AddChannelModal showModal={this.state.showModal} addChannel={(arg) => { }}>
 
           </AddChannelModal>
         </Row>
+
+        {_.map(this.state.supportingChannels, (el, idx) => {
+          return (<p>{el.site}</p>)
+        })}
       </div>
     </div>;
   }
@@ -116,7 +161,7 @@ class AnomaliesComponent extends React.Component<IAnomaliesComponentProps & IAno
 
 function mapStateToProps(state: IState) {
   return {
-    chartState: state.anomaliesScreen.chartState,
+    mainChartState: state.anomaliesScreen.chartState,
     gridState: state.anomaliesScreen.gridState,
     project: state.anomaliesScreen.project,
   };
