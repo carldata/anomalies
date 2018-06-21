10634c6125b3a8ddd6af579dd875b07832ebe8b6
sharp.coding <sharp.coding@gmail.com>
2018-06-21 18:03:05 +0200

    Issue_#159_Backless_development_with_express.js - all HTTP GET opertations work

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,35 @@
 // tslint:disable-next-line:no-var-requires
-const express = require('express');
+import express from 'express';
+import fs from 'fs';
+import { returnMockedJSON, returnMockedCsv } from './auxiliary';
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Idę do domu !'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.listen(3000, () => console.log('Idę do domu !'));
+app.get('/config/:appId', (req, res) => {
+  returnMockedJSON(res, 'assets/config.json');
+});
+
+app.get('/data/channel/:channelId/data', (req, res) => {
+  returnMockedCsv(res, `assets/data-${req.params.channelId}.csv`);
+});
+
+app.get('/anomalies/find', (req, res) => {
+  returnMockedCsv(res, `assets/find-${req.query.series}.csv`);
+});
+
+app.get('/data/site/:siteId', (req, res) => {
+  returnMockedJSON(res, 'assets/sites.json');
+});
+
+app.get('/data/channel/:channelId', (req, res) => {
+  returnMockedJSON(res, 'assets/channels.json');
+});
+
+// tslint:disable-next-line:no-console
+app.listen(3231, () => console.log('listening on port 3231'));
