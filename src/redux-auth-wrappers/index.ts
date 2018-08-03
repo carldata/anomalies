b764a70a497eff7d8f5a485a9ff84b70d56ae499
Tomasz Pichlak <tpichlak@gmail.com>
2018-08-03 09:46:43 +0200

    Minor change.

--- src/redux-auth-wrappers/index.ts
+++ src/redux-auth-wrappers/index.ts
@@ -4,10 +4,12 @@ import { IState } from '../state';
 
 export const isUserLoggedIn = connectedRouterRedirect({
     redirectPath: '/login',
+    allowRedirectBack: false,
     authenticatedSelector: (state: IState) => !_.isEmpty(state.configuration.token),
 });
 
 export const ifUserLoggedInRedirectToProjects = connectedRouterRedirect({
     redirectPath: '/projects',
+    allowRedirectBack: false,
     authenticatedSelector: (state: IState) => _.isEmpty(state.configuration.token),
 });
